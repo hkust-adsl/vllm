6b427a85d233825d7f405fa4affc04093c973ad3
ckchang@hfserver <cchangai@connect.ust.hk>
2025-06-04 03:06:35 +0000

    update Dockerfile, reduce the overhead of get_model_architecture

--- vllm/model_executor/model_loader/utils.py
+++ vllm/model_executor/model_loader/utils.py
@@ -6,6 +6,7 @@ import warnings
 from contextlib import contextmanager
 from dataclasses import dataclass, field
 from typing import Optional
+from functools import cache
 
 import torch
 import transformers
@@ -214,7 +215,7 @@ def resolve_transformers_arch(model_config: ModelConfig,
             architectures[i] = "TransformersForCausalLM"
     return architectures
 
-
+@cache
 def get_model_architecture(
         model_config: ModelConfig) -> tuple[type[nn.Module], str]:
     architectures = getattr(model_config.hf_config, "architectures", [])
