faf3e684f3ee4d20bed8b05de2f444b8b8e4f43b
ckchang@hfserver <cchangai@connect.ust.hk>
2025-06-06 07:14:49 +0000

    enable_prompt_tokens_details by default

--- vllm/entrypoints/openai/api_server.py
+++ vllm/entrypoints/openai/api_server.py
@@ -1209,7 +1209,7 @@ async def init_app_state(
         enable_auto_tools=args.enable_auto_tool_choice,
         tool_parser=args.tool_call_parser,
         reasoning_parser=args.reasoning_parser,
-        enable_prompt_tokens_details=args.enable_prompt_tokens_details,
+        enable_prompt_tokens_details=True,
     ) if model_config.runner_type == "generate" else None
     state.openai_serving_completion = OpenAIServingCompletion(
         engine_client,
